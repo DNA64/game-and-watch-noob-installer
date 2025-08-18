0c020a0ff2ebe5a8bfbc5ace92f7692bf1f399b1
viral_dna <DNA64@users.noreply.github.com>
2025-08-17 19:18:00 -0600

    Update lcdsetup.sh

--- lcdsetup.sh
+++ lcdsetup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-    # This script automates the setup of the Game & Watch Build Environment for Retro-Go.
-    # Copyright (C) 2021 DNA64 aka (viral_dna) / https://gist.github.com/DNA64
+# This script automates the setup of the Game & Watch Build Environment for Retro-Go.
+# Copyright (C) 2021 DNA64 aka (viral_dna) / https://gist.github.com/DNA64
 
-    # This program is free software: you can redistribute it and/or modify
-    # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation, either version 3 of the License, or
-    # (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-    # This program is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    # GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-    # You should have received a copy of the GNU General Public License
-    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    
-    # Logging
-    LOG_FILE="$HOME/lcdsetup.log"
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-    # ANSI color codes
-    RED='\033[0;31m'
-    YELLOW='\033[1;33m'
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
+# Logging
+LOG_FILE="$HOME/lcdsetup.log"
 
-    function log(){
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function log(){
     local level="$1"
     shift
     local message="$@"
@@ -80,47 +80,33 @@ function ensure_command(){
     fi
 }
 
-# Install Inkscape (PPA for stable, Build from source for interim)
+# Install Inkscape from source
 function gnw_install_inkscape(){
-    log INFO "Checking Ubuntu release..."
-    UBUNTU_CODENAME=$(lsb_release -c | awk '{print $2}')
-    STABLE_RELEASES=("focal" "jammy" "noble")
+    log INFO "Installing Inkscape from source..."
 
-    if [[ " ${STABLE_RELEASES[@]} " =~ " ${UBUNTU_CODENAME} " ]]; then
-        log INFO "Detected stable release: ${UBUNTU_CODENAME}"
-        log INFO "Installing Inkscape from PPA..."
-        ensure_command wget wget
-        sudo add-apt-repository -y ppa:inkscape.dev/stable-1.1
-        sudo apt-get update
-        sudo apt install -y inkscape
-    else
-        log INFO "Detected interim release: ${UBUNTU_CODENAME}"
-        log INFO "Installing Inkscape from source..."
+    # Ensure build tools
+    ensure_command wget wget
+    ensure_command cmake cmake
+    ensure_command make build-essential
 
-        # Ensure build tools
-        ensure_command wget wget
-        ensure_command cmake cmake
-        ensure_command make build-essential
+    # Install build dependencies
+    sudo apt update
+    sudo apt install -y pkg-config \
+        libgtk-3-dev libglib2.0-dev libpango1.0-dev \
+        libcairo2-dev libboost-dev libpoppler-dev \
+        libpoppler-glib-dev libgsl-dev libgc-dev \
+        libgtkmm-3.0-dev libxml2-dev libxslt1-dev \
+        libjpeg-dev libpng-dev libtiff-dev
 
-        # Install build dependencies
-        sudo apt update
-        sudo apt install -y pkg-config \
-            libgtk-3-dev libglib2.0-dev libpango1.0-dev \
-            libcairo2-dev libboost-dev libpoppler-dev \
-            libpoppler-glib-dev libgsl-dev libgc-dev \
-            libgtkmm-3.0-dev libxml2-dev libxslt1-dev \
-            libjpeg-dev libpng-dev libtiff-dev
-
-        # Download and build Inkscape
-        wget https://media.inkscape.org/dl/resources/file/inkscape-1.1.tar.xz
-        tar -xf inkscape-1.1.tar.xz
-        cd inkscape-1.1
-        mkdir build && cd build
-        cmake ..
-        make -j$(nproc)
-        sudo make install
-        cd ../..
-    fi
+    # Download and build Inkscape
+    wget https://media.inkscape.org/dl/resources/file/inkscape-1.1.tar.xz
+    tar -xf inkscape-1.1.tar.xz
+    cd inkscape-1.1
+    mkdir build && cd build
+    cmake ..
+    make -j$(nproc)
+    sudo make install
+    cd ../..
 }
 
 # ─────────────────────────────────────────────────────────────
@@ -143,13 +129,13 @@ function gnw_clone_lcdgs(){
 # ─────────────────────────────────────────────────────────────
 # Prompt User to Install LCD-Game-Shrinker
 function lcd_game_shrinker(){
-    echo ""
-    echo "Do you want to install bzhxx's LCD-Game-Shrinker and its dependencies?"
-    echo "https://github.com/bzhxx/LCD-Game-Shrinker"
-    echo ""
-    echo "This is required for LCD handheld devices (Game & Watch, Konami, Tiger, Elektronika,...)."
-    echo ""
-    echo "Please enter 1 for Yes, or 2 for No.."
+    log INFO ""
+    log INFO "Do you want to install bzhxx's LCD-Game-Shrinker and its dependencies?"
+    log INFO "https://github.com/bzhxx/LCD-Game-Shrinker"
+    log INFO ""
+    log INFO "This is required for LCD handheld devices (Game & Watch, Konami, Tiger, Elektronika,...)."
+    log INFO ""
+    log INFO "Please enter 1 for Yes, or 2 for No.."
     select yn in "Yes" "No"; do
         case $yn in
             Yes ) gnw_clone_lcdgs; break;;
